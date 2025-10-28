daf5a986fe510f02d2e44011068881af6a21d6ee
sajid akram <sajidwarner@gmail.com>
2025-10-28 13:53:35 +0600

    componentes day 2

---
+++ day-02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+    <div className='card'>
+    <h1>This Is Sajid</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, vitae!</p>
+  </div>
+</div>
+  )
+}
+
+export default Card
---
+++ day-02-components/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <div className="nav">
+            This is nav
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
