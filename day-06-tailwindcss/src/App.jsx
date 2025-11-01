732d03a1780d7c37e28e4787f20bb0109718373d
sajid akram <sajidwarner@gmail.com>
2025-11-01 18:45:51 +0600

    tailwindcss

---
+++ day-06-tailwindcss/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' h-screen w-full bg-gray-600 text-white p-4'>
+      hi tailwindcss
+    </div>
+  )
+}
+
+export default App
